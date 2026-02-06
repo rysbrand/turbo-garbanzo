600f5709f4bb692ba5218dffd7f6b1b2fbd0c0ab
dakaleeb <deltasquadgamer@gmail.com>
2026-02-06 11:04:13 -0600

    Add files via upload

---
+++ src/temp/view/temp.js
@@ -0,0 +1,7 @@
+const navToggle = document.getElementById("navToggle");
+const NavId = document.getElementById("navId");
+
+navToggle.addEventListener("click", function() {
+    NavId.classList.toggle("show");
+});
+
